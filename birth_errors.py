9f06a19797bc6a03bb15734abbff6e55b2efc764
Matt Dalton <mdalton142@gmail.com>
2019-02-12 10:28:09 -0500

    Created db module, additional cleanup. NOT TESTED

--- birth_errors.py
+++ birth_errors.py
@@ -3,13 +3,11 @@
 
 import os
 import csv
-from collections import Counter # Still needed? it is used in prod code
 
 from bs4 import BeautifulSoup
 
-import psycopg2
-
-
+from dwg_errors_DB import (pull_parts, pull_error_codes, update_error_codes, 
+						   update_DB)
 
 
 def find_error_htmls(root):
@@ -25,29 +23,6 @@ def find_error_htmls(root):
 				error_htmls.append(os.path.join(dirpath, dirfile))
 
 	return error_htmls
-
-def init_scrape(filename):
-	"""     TEST. TEST TEST
-	Just using for test. To pull out the error code test."""
-	# Open the file, create the soup, parse out the data. 
-	with open(filename, 'r') as htmlfile:
-		contents = htmlfile.read()
-		# Q?: Can a regex be used here instead? 
-	# Make soup
-	soup = BeautifulSoup(contents, 'html.parser')
-	# Pull out all td's w/ att and print
-	tds = soup.find_all('td', width = "550")
-	# show it 
-	
-	# init counter
-	counter = 0
-	for td in tds[1:]:
-		# print(i.text)
-		len_text = len(td.text)
-		if len_text > counter:
-			counter = len_text
-	# print()
-	return counter 
 
 def build_error_table(filename):
 	"""
@@ -66,7 +41,7 @@ def build_error_table(filename):
 	codes = {}
 	for tr in trs:
 		# Pull just the a.text
-		error_code = tr.a.text # Works. Could have searched on td
+		error_code = tr.a.text # Can also search on td
 		error_msg = tr.find('td', width = "550").text
 		if error_code not in codes:
 			codes[error_code] = error_msg
@@ -79,7 +54,7 @@ def write_errors_csv(full_codes):
 	Writes out (CWD) error codes dcny to a csv for SQL import.
 	"""
 	# Passing in dcny, so simple reader is enough
-	with open('birth_codes.csv', 'w', newline = '') as csvfile:
+	with open('error_codes.csv', 'w', newline = '') as csvfile:
 		csvwriter = csv.writer(csvfile, delimiter = ',', 
 			quotechar = '"')
 		# Write header
@@ -88,7 +63,7 @@ def write_errors_csv(full_codes):
 		for key, value in full_codes.items():
 			csvwriter.writerow([key, value])
 	
-def scrape_ind_html(filename, full_errors, full_parts):
+def scrape_ind_html(filename, error_codes, parts_list):
 	"""
 	Full scrape of the file of:
 	part number, part extension, date, and list of error codes.
@@ -120,186 +95,69 @@ def scrape_ind_html(filename, full_errors, full_parts):
 	part_full = trs[0].find('td', width="750").text
 	part_num, part_ext = os.path.splitext(part_full)
 	# Check if part_num in the full_part list
-	if part_num in full_parts:
+	if part_num in parts_list:
 		print('{} already in DB.'.format(part_num))
 		return None, ''
 	
 	# date. 
-	# Does this need updated from string? DB should handle...
+	# DB will convert to datetime...
 	date = trs[2].find('td', width="750").text
 		
-	# Second table 
+	# Second table contains errors and error messages
 	trs = tables[1].find_all('tr', bgcolor = "#ffffff")
-	# init ind error list
+	# init ind error list. This will be passed with the full part details
 	error_list = []
-	# iterate through, check if code exists. 
-	err_update = {}
+	# Collect codes and check for new ones. 
+	error_update = {}
 	for tr in trs:
 		# Pull a.text for error code 
 		error_code = tr.a.text
-		if error_code not in full_errors:
-			# function for importing into DB list. 
+		if error_code not in error_codes:
 			print('Found code in {}.'.format(filename))
-			# Need to return the error code and its msg.
-			# CREATE  as tuple, for the DB input. 
-			# Although this can return multiple codes per html
-			err_update[error_code] = tr.find('td', width = "550").text
-		# continue by adding the error code to the list. 
+			# Capture msg as well
+			error_update[error_code] = tr.find('td', width = "550").text
+
 		error_list.append(error_code)
 		
 	# populate dcny, then return 
-	dcny = {'part_num' : part_num,
+	part_data = {'part_num' : part_num,
 			'part_ext' : part_ext,
 			'date': date,
 			'errors': error_list
 		}
 		
-	return dcny, err_update
-
-def pull_error_codes():
-	"""
-	Pulls codes from the birth_errors postgresql table.
-	returns list of these codes. 
-	"""
-	# Make connection
-	conn = psycopg2.connect(database = 'test', 
-							user = 'postgres', 
-							password = 'Strider142'
-						)
-	# Create cursor object 
-	cur = conn.cursor()
-	# Make query 
-	cur.execute('SELECT error_code FROM birth_errors')
-	# return the rows of query
-	rows = cur.fetchall()
-	# create list to return. fetchall() creates a tuple.
-	full_errors = list(row[0] for row in rows)
-	# Shut it down
-	cur.close()
-	conn.close()
-	
-	return full_errors
-	
-def update_error_codes(dcny): 	
-	"""
-	Write updated dcny to DB.
-	Takes dcny of new error codes and updates appropriate table in DB.
-	"""
-	# connect to, write out dcny 
-	conn = psycopg2.connect(database = 'test', 
-							user = 'postgres',
-							password = 'Strider142',
-						)
-	cur = conn.cursor()
-	# Create statement. Note no semi-colon
-	sql = '''INSERT INTO birth_errors 
-			 VALUES 
-				(%s, %s)
-		  '''
-	# Convert dcny to list of tuples for execute many stmt below.
-	error_list = list((key, value) for key, value in dcny.items())
-	cur.executemany(sql, error_list)
-	# Commit and close out. 
-	conn.commit()
-	cur.close()
-	conn.close()
-
-def pull_parts():
-	"""
-	Grabs all parts from the DB. Used to compare new entries from dir.
-	"""
-	
-	conn = psycopg2.connect(database = 'test', 
-							user = 'postgres',
-							password = 'Strider142',
-						)
-	cur = conn.cursor()
-	# Get the part numbers. 
-	cur.execute('SELECT part_number FROM parts')
-	# Returns list of tuples from query 
-	rows = cur.fetchall()
-	# create a list to return 
-	parts = list(row[0] for row in rows)
-	
-	return parts 
-
-def final_db_write(data, table):
-	""" 
-	Writes the list of tuples into the table specified by arg
-	"""
-	
-	# Connect to DB 
-	conn = psycopg2.connect(database = 'test', user = 'postgres', 
-		password = 'Strider142')
-	cur = conn.cursor()
-	# Create query 
-	sql = 'INSERT INTO %s' % table
-	sql += '''\nVALUES
-		(%s, %s, %s)
-		'''
-	# Execute stmt and commit
-	cur.executemany(sql, data)
-	conn.commit()
-	# Close out 
-	cur.close()
-	conn.close()
-	
-		
-def update_DB(full_list):
-	"""
-	Takes the full list from html scrapes and breaks up into two lists.
-	These are then imported into the parts and dwg_details tables in DB.
-	"""
-	
-	# first_table == 'parts' 
-	# Update 01302019: list comprehension looks ugly here- clean up.
-	first_table = list((row['part_num'],
-						row['part_ext'],
-						row['date']) for row in full_list
-					)
-					
-	# second_table == dwg_details table			
-	second_table = []
-	for row in full_list:
-		counted = Counter(row['errors'])
-		for key, value in counted.items():
-			second_table.append((row['part_num'], key, value))
-		
-	# Pass both lists (of tuples) to functions to write to DB. 
-	final_db_write(first_table, 'parts')
-	final_db_write(second_table, 'dwg_details')
-	
+	return part_data, error_update
 	
 	
 if __name__ == '__main__':
 	
 	root = 'S:\\!RPA!\\BIRTH'
-	# Get current error_list from DB 
-	full_errors = pull_error_codes()
-	# Get current parts list
-	full_parts = pull_parts()
+	# Get current error codes and parts from DB 
+	error_codes = pull_error_codes()
+	parts_list = pull_parts()
 	
 	# Find all error htmls in root.
 	error_htmls = find_error_htmls(root)
 	# Show length 
 	print(len(error_htmls))
 	
-	full_list = []	
+	# init list for ind part dcny's
+	full_list= []	
 	for html in error_htmls:
-		print(html)
-		dcny, err_update = scrape_ind_html(html, full_errors, 
-			full_parts)
+		print(html) 
+		part_data, error_updates = scrape_ind_html(html, error_codes, 
+			parts_list)
 		# append the ind scrape dcny to list 
 		# Check saves and 3DDSS are returned as None, so can be ignored
-		if dcny != None:
-			full_list.append(dcny)
+		if part_data != None:
+			full_list.append(part_data)
 		# Update full_errors if necessary 
-		if err_update:
+		if error_updates:
 			# Will run code to update and return the full list. 
 			print('Updating DB error list...')
-			update_error_codes(err_update)
+			update_error_codes(error_updates)
 			# Need to pull the list after updates. 
-			full_errors = pull_error_codes()		
+			error_codes = pull_error_codes()		
 			
 	print()	
 	### TEST ### 
---
+++ dwg_errors_DB.py
@@ -0,0 +1,129 @@
+# dwg_errors_DB.py
+# 02.12.2019
+
+from collections import Counter
+import psycopg2
+
+
+# NOTE: 
+# Password to be entered by user...
+
+def pull_parts(password = 'Strider142'):
+	"""
+	Returns list of parts from DB. 
+	"""
+	conn = psycopg2.connect(database = 'test', 
+							user = 'postgres',
+							password = password,
+						)
+	cur = conn.cursor()
+	# Get the part numbers. 
+	cur.execute('SELECT part_number FROM parts')
+	# Returns list of tuples from query 
+	rows = cur.fetchall()
+	# create a list to return 
+	parts = list(row[0] for row in rows)
+	
+	return parts 
+
+def pull_error_codes(password = 'Strider142'):
+	"""
+	Returns list of dwg error codes from DB.
+	"""
+	conn = psycopg2.connect(database = 'test', 
+							user = 'postgres', 
+							password = password,
+						)
+	# Create cursor object 
+	cur = conn.cursor()
+	# Make query 
+	cur.execute('SELECT error_code FROM dwg_errors')
+	# return the rows of query
+	rows = cur.fetchall()
+	# create list to return. fetchall() creates a tuple.
+	full_errors = list(row[0] for row in rows)
+	# Shut it down
+	cur.close()
+	conn.close()
+	
+	return full_errors
+
+def update_error_codes(dcny, password = 'Strider142'): 	
+	"""
+	Write updated dcny to DB.
+	Takes dcny of new error codes and updates appropriate table in DB.
+	"""
+	conn = psycopg2.connect(database = 'test', 
+							user = 'postgres',
+							password = password,
+						)
+	cur = conn.cursor()
+	# Create statement. Note no semi-colon
+	sql = '''INSERT INTO dwg_errors 
+			 VALUES 
+				(%s, %s)
+		  '''
+	# Convert dcny to list of tuples for execute many stmt below.
+	error_list = list((key, value) for key, value in dcny.items())
+	cur.executemany(sql, error_list)
+	# Commit and close out. 
+	conn.commit()
+	cur.close()
+	conn.close()	
+
+def final_db_write(data, table, password = 'Strider142'):
+	""" 
+	Writes the list of tuples into the table specified by arg
+	"""	
+	conn = psycopg2.connect(database = 'test', 
+							user = 'postgres', 
+							password = password,
+						)
+	cur = conn.cursor()
+	# Create query. Works for both parts and dwg_details tables
+	sql = 'INSERT INTO %s' % table
+	sql += '''\nVALUES
+		(%s, %s, %s)
+		'''
+	# Execute stmt and commit
+	cur.executemany(sql, data)
+	conn.commit()
+	# Close out 
+	cur.close()
+	conn.close()		
+	
+def update_DB(full_list):
+	"""
+	Takes the full list from html scrapes and breaks up into two lists.
+	These are then imported into the parts and dwg_details tables in DB.
+	"""	
+	# first_table == 'parts' 
+	first_table = list((row['part_num'],
+						row['part_ext'],
+						row['date']) for row in full_list
+					)	
+					
+	# second_table == dwg_details table
+	# The block below creates a list of tuples that contain each individual
+	# error and the number of times that error appears in the dwg			
+	second_table = []
+	for row in full_list:
+		counted = Counter(row['errors'])
+		for key, value in counted.items():
+			second_table.append((row['part_num'], key, value))
+		
+	# Pass both lists (of tuples) to functions to write to DB. 
+	final_db_write(first_table, 'parts')
+	final_db_write(second_table, 'dwg_details')
+	
+
+	
+
+
+	
+	
+	
+
+
+
+	
